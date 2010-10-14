564c5138f04893ba332d2b239499e12508570db7
Mark John Buenconsejo <mjwork@simpleteq.com>
2010-10-14 19:22:11 +0800

    removed unnecessary codes, that exists in the legacy version

--- lib/palmade/db_essentials/helpers.rb
+++ lib/palmade/db_essentials/helpers.rb
@@ -1,5 +1,14 @@
 module Palmade::DbEssentials
   module Helpers
+    def self.use(helper_name, *args)
+      mod = const_get(helper_name)
+      if args.size > 0
+        mod.setup(*args)
+      else
+        mod
+      end
+    end
+
     autoload :ActiveRecord2Helper, File.join(DB_ESSENTIALS_LIB_DIR, 'db_essentials/helpers/active_record2_helper')
   end
 end
